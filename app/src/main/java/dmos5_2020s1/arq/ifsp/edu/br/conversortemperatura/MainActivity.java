32ee8fb3960db87f4d404d03e7ab19a423eee3fb
vinicius.montouro <vinicius.montouro@castgroup.com.br>
2020-03-08 22:29:31 -0300

    Versão 2

--- app/src/main/java/dmos5_2020s1/arq/ifsp/edu/br/conversortemperatura/MainActivity.java
+++ app/src/main/java/dmos5_2020s1/arq/ifsp/edu/br/conversortemperatura/MainActivity.java
@@ -10,35 +10,61 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private EditText valorTemperaturaFahrenheit;
-    private Button converter;
+    private EditText valorTemperatura;
+    private Button converterCelsius;
+    private Button converterFahrenheit
     private TextView temperaturaConvertida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        valorTemperaturaFahrenheit = findViewById(R.id.editText_valor_temperatura);
-        converter = findViewById(R.id.button_converter);
-        temperaturaConvertida = findViewById(R.id.text_view_valor_celsus);
-        converter.setOnClickListener(this);
+        valorTemperatura = findViewById(R.id.editText_valor_temperatura);
+        converterCelsius = findViewById(R.id.button_converter_celsius);
+        converterFahrenheit = findViewById(R.id.button_converter_fahrenheit);
+        temperaturaConvertida = findViewById(R.id.text_view_valor_convertido);
+        converterCelsius.setOnClickListener(this);
+        converterFahrenheit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v.equals(converter)){
+        if(v.equals(converterCelsius)){
             converterFahrenheitToCelsius();
+        }else{
+            converterCelsiusToFahrenheit();
         }
     }
 
-    public void converterFahrenheitToCelsius(){
-        double entrada = 0;
+    private double getEntrada() throws NumberFormatException{
+        double entrada;
+
         try{
-            entrada = Double.parseDouble(valorTemperaturaFahrenheit.getText().toString());
+            entrada = Double.valueOf(valorRealEditText.getText().toString());
+        }catch(NumberFormatException e){
+            entrada = 0;
+            throw e;
+        }
+        return entrada;
+    }
+
+    public void converterFahrenheitToCelsius(){
+        double valor = 0;
+        try{
+            valor = getEntrada();
         }catch(NumberFormatException e){
             Toast.makeText(this, "Valor digitado é inválido", Toast.LENGTH_SHORT).show();
         }
         temperaturaConvertida.setText(String.format("%.2f ºC", (entrada - 32)/1.8));
     }
 
+    public void converterCelsiusToFahrenheit(){
+        double valor = 0;
+        try{
+            valor = getEntrada();
+        }catch(NumberFormatException e){
+            Toast.makeText(this, "Valor digitado é inválido", Toast.LENGTH_SHORT).show();
+        }
+        temperaturaConvertida.setText(String.format("%.2f ºF", (entrada * 1.8)+32));
+    }
 }
